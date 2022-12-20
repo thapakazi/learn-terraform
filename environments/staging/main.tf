41658f7ae0585068b2718f0978915dc753ee24fa
thapakazi <thapakazi@m1.local>
2022-12-20 16:09:49 +0545

    secrets inherit: quote secrets - self hosted with - tf cloud debug

--- environments/staging/main.tf
+++ environments/staging/main.tf
@@ -1,3 +1,15 @@
+terraform {
+  cloud {
+    organization = "cloudrickshaw"
+    hostname     = "app.terraform.io"
+
+    workspaces {
+      #tags = ["region:us-east-1", "env:staging"]
+      name = "myapp-staging"
+    }
+  }
+}
+
 # changes made on staging 
 module "api" {
   source = "../../modules/api"
